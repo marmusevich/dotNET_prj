705d1a675189c369e700575ff2f7f3b37f5295f0
marmusevich <myrmik@gmail.com>
2017-06-13 14:48:36 +0300

    new

---
+++ Upwork_MiniBattleShip/Upwork_MiniBattleShip/frmMain.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace Upwork_MiniBattleShip
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnlShips = new System.Windows.Forms.Panel();
+            this.lblGamaResult = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // pnlShips
+            // 
+            this.pnlShips.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pnlShips.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pnlShips.Location = new System.Drawing.Point(13, 13);
+            this.pnlShips.Name = "pnlShips";
+            this.pnlShips.Padding = new System.Windows.Forms.Padding(3);
+            this.pnlShips.Size = new System.Drawing.Size(319, 249);
+            this.pnlShips.TabIndex = 0;
+            this.pnlShips.ClientSizeChanged += new System.EventHandler(this.pnlShips_ClientSizeChanged);
+            // 
+            // lblGamaResult
+            // 
+            this.lblGamaResult.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblGamaResult.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblGamaResult.Location = new System.Drawing.Point(13, 265);
+            this.lblGamaResult.Name = "lblGamaResult";
+            this.lblGamaResult.Size = new System.Drawing.Size(319, 41);
+            this.lblGamaResult.TabIndex = 1;
+            this.lblGamaResult.Text = "lblGamaResult";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.btnStart.Location = new System.Drawing.Point(135, 323);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 2;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(344, 352);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.lblGamaResult);
+            this.Controls.Add(this.pnlShips);
+            this.MaximumSize = new System.Drawing.Size(720, 780);
+            this.MinimumSize = new System.Drawing.Size(360, 390);
+            this.Name = "frmMain";
+            this.Text = "Mini BattleShip";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlShips;
+        private System.Windows.Forms.Label lblGamaResult;
+        private System.Windows.Forms.Button btnStart;
+
+    }
+}
+
---
+++ Upwork_MiniBattleShip/Upwork_MiniBattleShip/frmMain.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Upwork_MiniBattleShip
+{
+    public partial class frmMain : Form
+    {
+        enum ShipType
+        {
+            None = 0,
+            Vertical = 1,
+            Horizontal = 2
+        }
+
+        class FieldDiscriptions
+        {
+            public bool isCliced = false;
+            public ShipType shipType = ShipType.None;
+        }
+
+
+        // game size
+        const int SizeX = 4;
+        const int SizeY = 4;
+
+        // 
+        const int dx = 4;
+        const int dy = 4;
+
+
+        //colors
+        Color blank = SystemColors.ControlDark;
+        Color see = Color.Blue;
+        Color shipHorizontal = Color.Red;
+        Color shipVertical = Color.Black;
+
+
+        int clickCount = 0;
+
+
+        Label[,] lblField = new Label[SizeX, SizeY];
+
+
+        private void createField()
+        {
+            pnlShips.SuspendLayout();
+            for (int i = 0; i < SizeX; i++)
+            {
+                for (int j = 0; j < SizeY; j++)
+                {
+                    lblField[i, j] = new Label();
+                    lblField[i, j].BackColor = blank;
+                    lblField[i, j].BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+                    lblField[i, j].Name = "lblField" + i.ToString() + "_" + j.ToString();
+                    lblField[i, j].TabIndex = i + j * SizeX;
+                    lblField[i, j].Click += new System.EventHandler(this.lblField_Click);
+                    lblField[i, j].Tag = fd[i, j];
+
+                }
+            }
+            setFielsSize();
+
+            pnlShips.ResumeLayout(false);
+            pnlShips.PerformLayout();
+        }
+
+
+        private void setFielsSize()
+        {
+            int sx = pnlShips.ClientSize.Width / SizeX - dx;
+            int sy = pnlShips.ClientSize.Height / SizeY - dy;
+
+            for (int i = 0; i < SizeX; i++)
+            {
+                for (int j = 0; j < SizeY; j++)
+                {
+                    lblField[i, j].Location = new Point(sx * i + dx + dx * i, sy * j + dy + dy * j);
+                    lblField[i, j].Size = new Size(sx, sy);
+                }
+            }
+        }
+
+
+
+
+        FieldDiscriptions[,] getMap()
+        {
+            FieldDiscriptions[,] fd = new FieldDiscriptions[SizeX, SizeY];
+
+            for (int i = 0; i < SizeX; i++)
+            {
+                for (int j = 0; j < SizeY; j++)
+                {
+                    fd[i, j] = new FieldDiscriptions();
+                }
+            }
+
+            fd[1, 0].shipType = ShipType.Vertical;
+            fd[1, 1].shipType = ShipType.Vertical;
+
+            fd[2, 3].shipType = ShipType.Horizontal;
+            fd[3, 3].shipType = ShipType.Horizontal;
+
+            return fd;
+        }
+
+        private void startGame()
+        {
+            FieldDiscriptions[,] fd = getMap();
+
+            for (int i = 0; i < SizeX; i++)
+            {
+                for (int j = 0; j < SizeY; j++)
+                {
+                    lblField[i, j].Tag = fd[i, j];
+                }
+            }
+
+
+            clickCount = 0;
+        }
+
+
+        private void chekField(Label lbl)
+        {
+            FieldDiscriptions fd = lbl.Tag as FieldDiscriptions;
+
+            if (fd != null && !fd.isCliced)
+            {
+                fd.isCliced = true;
+                clickCount++;
+
+                if (fd.shipType == ShipType.Horizontal)
+                    lbl.BackColor = shipHorizontal;
+                else if (fd.shipType == ShipType.Vertical)
+                    lbl.BackColor = shipVertical;
+                else
+                    lbl.BackColor = see;
+
+            }
+        }
+
+
+        //---------------------------------------------------------------------
+
+
+
+        public frmMain()
+        {
+            InitializeComponent();
+
+            createField();
+
+            startGame();
+        }
+
+
+
+        private void lblField_Click(object sender, EventArgs e)
+        {
+            Label lbl = (Label)sender;
+            if (lbl != null)
+                chekField(lbl);
+        }
+
+
+        private void pnlShips_ClientSizeChanged(object sender, EventArgs e)
+        {
+            setFielsSize();
+        }
+    }
+}
